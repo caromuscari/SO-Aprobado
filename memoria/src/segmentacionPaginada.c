c4a05ca414a8fe684fedfd6c04d35c05f08804ff
giulivetere <giulianavetere98@gmail.com>
2019-06-12 20:05:37 -0300

    agrego archivos de segmentacion paginada

---
+++ memoria/src/segmentacionPaginada.c
@@ -0,0 +1,8 @@
+/*
+ * segmentacionPaginada.c
+ *
+ *  Created on: 12 jun. 2019
+ *      Author: utnso
+ */
+
+#include "segmentacionPaginada.h"
---
+++ memoria/src/segmentacionPaginada.h
@@ -0,0 +1,13 @@
+/*
+ * segmentacionPaginada.h
+ *
+ *  Created on: 12 jun. 2019
+ *      Author: utnso
+ */
+
+#ifndef SRC_SEGMENTACIONPAGINADA_H_
+#define SRC_SEGMENTACIONPAGINADA_H_
+
+
+
+#endif /* SRC_SEGMENTACIONPAGINADA_H_ */

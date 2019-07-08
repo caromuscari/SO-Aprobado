1b66b3cd60ba43b446f5ac67a22b415a227e075a
utnso <marcerajaa@gmail.com>
2019-07-08 11:13:15 -0300

    Arregla timestamp

--- funcionesCompartidas/funcionesCompartidas/time.c
+++ funcionesCompartidas/funcionesCompartidas/time.c
@@ -7,5 +7,7 @@
 double obtenerMilisegundosDeHoy(){
     struct timeval tv;
     gettimeofday(&tv, NULL);
-    return (tv.tv_sec) * 1000 + (float)tv.tv_usec / 1000;
+    unsigned long long result = (((unsigned long long)tv.tv_sec) * 1000 + ((unsigned long long)tv.tv_usec)/1000);
+    double a = result;
+    return a;
 }

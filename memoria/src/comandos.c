69052defeef62341869d252ab3ef9024f47e3546
miguel <miguel.sanchez528@gmail.com>
2019-08-01 02:16:28 -0300

    adding: login

--- memoria/src/comandos.c
+++ memoria/src/comandos.c
@@ -12,99 +12,90 @@ extern t_configuracionMemoria* configMemoria;
 //COMANDO INSERT
 int comandoInsert(st_insert* comandoInsert){
 	if(strlen(comandoInsert->value) > tamanioValue){
-		log_error(file_log, "El value es mayor al tamaño máximo");
+		log_error(file_log, "[proceso Insert]El value es mayor al tamaño máximo");
 		return MAYORQUEVALUEMAX;
 	}
-	pthread_mutex_lock(&mutexMemPrinc);
+	log_info(file_log,"[proceso Insert] Iniciando proceso");
+    pthread_mutex_lock(&mutexMemPrinc);
 	st_segmento* segmentoEncontrado = buscarSegmentoPorNombreTabla(comandoInsert->nameTable);//devuelve el segmento con ese nombre de tabla
 	if(segmentoEncontrado){
-		log_info(file_log, "Segmento encontrado por comando Insert");
-
+		log_info(file_log, "[proceso Insert]Segmento encontrado");
 		st_tablaDePaginas* paginaDeTablaEncontrada = buscarPaginaPorKey(segmentoEncontrado->tablaDePaginas, comandoInsert->key);
-
 		if(paginaDeTablaEncontrada){
-			log_info(file_log, "Pagina encontrada");
+			log_info(file_log, "[proceso Insert]Pagina encontrada");
+            log_info(file_log, "[proceso Insert]Actualizando pagina");
 			memcpy(paginaDeTablaEncontrada->pagina + sizeof(double) + sizeof(uint16_t), comandoInsert->value, tamanioValue);
 			memcpy(paginaDeTablaEncontrada->pagina, &comandoInsert->timestamp, sizeof(double));
 			paginaDeTablaEncontrada->flagModificado = 1;
-
-			log_info(file_log, "El Insert se realizo correctamente");
-
+            log_info(file_log,"[proceso Insert] Finalizar proceso");
             sleep(configMemoria->RETARDO_MEM/1000);
             pthread_mutex_unlock(&mutexMemPrinc);
 			return OK;
 		}
-		log_info(file_log, "No se encontro la pagina con esa Key");
-		int posMarcoLibre = buscarMarcoLibre();
+        log_info(file_log, "[proceso Insert]No se encontro pagina");
+        log_info(file_log, "[proceso Insert]Buscando Disponibilidad en la memoria");
+        int posMarcoLibre = buscarMarcoLibre();
 		if(posMarcoLibre == -1){
-			log_info(file_log, "Memoria full por insert");
-			pthread_mutex_unlock(&mutexMemPrinc);
+			log_info(file_log, "proceso Insert]Memoria full");
+            log_info(file_log,"[proceso Insert] Finalizar proceso");
+            pthread_mutex_unlock(&mutexMemPrinc);
             return FULLMEMORY;
 		}
-		void* paginaLibre = memoriaPrincipal + (posMarcoLibre * (sizeof(double) + sizeof(uint16_t) + tamanioValue));
-
+        log_info(file_log, "[proceso Insert]Cargando Nueva Pagina");
+        void* paginaLibre = memoriaPrincipal + (posMarcoLibre * (sizeof(double) + sizeof(uint16_t) + tamanioValue));
 		memcpy(paginaLibre, &comandoInsert->timestamp, sizeof(double));
 		memcpy(paginaLibre + sizeof(double), &comandoInsert->key, sizeof(uint16_t));
-
 		int largo_value = strlen(comandoInsert->value);
 		comandoInsert->value[largo_value]='\0';
-
 		memcpy(paginaLibre + sizeof(double) + sizeof(uint16_t), comandoInsert->value, tamanioValue);
 
 		st_tablaDePaginas* paginaDeTabla = malloc(sizeof(st_tablaDePaginas));
 		paginaDeTabla->nroDePagina = posMarcoLibre;
 		paginaDeTabla->pagina = paginaLibre;
-		//paginaDeTabla->desplazamiento =string_length(comandoInsert->value);
 		paginaDeTabla->flagModificado = 1;
 
-		list_add(segmentoEncontrado->tablaDePaginas, paginaDeTabla);
-
+        log_info(file_log, "[proceso Insert]Agregando nueva pagina al segmento");
+        list_add(segmentoEncontrado->tablaDePaginas, paginaDeTabla);
 		st_marco* marco = list_get(listaDeMarcos, posMarcoLibre);
 		marco->condicion = OCUPADO;
-        log_info(file_log, "El Insert se realizo correctamente");
+        log_info(file_log,"[proceso Insert] Finalizar proceso");
         sleep(configMemoria->RETARDO_MEM/1000);
         pthread_mutex_unlock(&mutexMemPrinc);
 		return OK;
 	}
 
-	log_info(file_log, "No se encontro el segmento de esa tabla");
-	//creo el segmento
-	st_segmento* segmentoNuevo= malloc(sizeof(st_segmento));
-
-	segmentoNuevo->nombreTabla = strdup(comandoInsert->nameTable);
-	segmentoNuevo->tablaDePaginas = list_create();
-
-	int posMarcoLibre = buscarMarcoLibre();
+	log_info(file_log, "[proceso Insert]No se encontro segmento");
+    log_info(file_log, "[proceso Insert]Buscando Disponibilidad en la memoria");
+    int posMarcoLibre = buscarMarcoLibre();
 	if(posMarcoLibre == -1){
-		log_info(file_log, "Memoria full insert");
-		pthread_mutex_unlock(&mutexMemPrinc);
+        log_info(file_log, "proceso Insert]Memoria full");
+        log_info(file_log,"[proceso Insert] Finalizar proceso");
+        pthread_mutex_unlock(&mutexMemPrinc);
         return FULLMEMORY;
 	}
-	//creo la pagina
-	void* paginaLibre = memoriaPrincipal + (posMarcoLibre * (sizeof(double) + sizeof(uint16_t) + tamanioValue));
-	//cargo datos a la memoria princ
+    log_info(file_log, "[proceso Insert]Creando Nuevo Segmento");
+    st_segmento* segmentoNuevo= malloc(sizeof(st_segmento));
+    segmentoNuevo->nombreTabla = strdup(comandoInsert->nameTable);
+    segmentoNuevo->tablaDePaginas = list_create();
+    log_info(file_log, "[proceso Insert]Cargando Nueva Pagina");
+    void* paginaLibre = memoriaPrincipal + (posMarcoLibre * (sizeof(double) + sizeof(uint16_t) + tamanioValue));
 	memcpy(paginaLibre, &comandoInsert->timestamp, sizeof(double));
 	memcpy(paginaLibre + sizeof(double), &comandoInsert->key, sizeof(uint16_t));
-
 	int largo_value = strlen(comandoInsert->value);
 	comandoInsert->value[largo_value]='\0';
-
 	memcpy(paginaLibre + sizeof(double) + sizeof(uint16_t), comandoInsert->value, tamanioValue);
-	//creo la pag de la tabla y le cargo los datos
 	st_tablaDePaginas* paginaDeTabla = malloc(sizeof(st_tablaDePaginas));
 	paginaDeTabla->nroDePagina = posMarcoLibre;
 	paginaDeTabla->pagina = paginaLibre;
-    //paginaDeTabla->desplazamiento =string_length(comandoInsert->value);
 	paginaDeTabla->flagModificado = 1;
-	//agrego la pag a la lista
+    log_info(file_log, "[proceso Insert]Agregando nueva pagina al segmento");
 	list_add(segmentoNuevo->tablaDePaginas, paginaDeTabla);
-	//agrego el segmento a la lista de segmentos en el ultimo lugar
+    log_info(file_log, "[proceso Insert]Agregando nueva segmento a la lista de segmentos");
 	segmentoNuevo->nroSegmento = list_size(listaDeSegmentos);
 	list_add_in_index(listaDeSegmentos, segmentoNuevo->nroSegmento, segmentoNuevo);
-
 	st_marco* marco = list_get(listaDeMarcos, posMarcoLibre);
 	marco->condicion = OCUPADO;
-	log_info(file_log, "El Insert se realizo correctamente");
+    log_info(file_log,"[proceso Insert] Finalizar proceso");
     sleep(configMemoria->RETARDO_MEM/1000);
     pthread_mutex_unlock(&mutexMemPrinc);
 	return OK;
@@ -112,16 +103,17 @@ int comandoInsert(st_insert* comandoInsert){
 
 // COMANDO SELECT
 st_registro* comandoSelect(st_select* comandoSelect, enum_resultados* resultado){
-	st_segmento* segmentoEncontrado;
+    log_info(file_log,"[proceso Select] Iniciando Proceso");
+    st_segmento* segmentoEncontrado;
 	st_registro* registro;
 	pthread_mutex_lock(&mutexMemPrinc);
 	segmentoEncontrado = buscarSegmentoPorNombreTabla(comandoSelect->nameTable);//devuelve el segmento con ese nombre de tabla
 
 	if(segmentoEncontrado){
-		log_info(file_log, "[proces Select] Segmento encontrado");
+		log_info(file_log, "[proceso Select] Segmento encontrado");
 		st_tablaDePaginas* paginaDeTablaEncontrada = buscarPaginaPorKey(segmentoEncontrado->tablaDePaginas, comandoSelect->key);
 		if(paginaDeTablaEncontrada){
-			log_info(file_log, "[proces Select] Se Encontro Pagina");
+			log_info(file_log, "[proceso Select] Se Encontro Pagina");
 			registro = malloc(sizeof(st_registro));
             registro->value = malloc(tamanioValue);
 			st_marco* marco = list_get(listaDeMarcos, paginaDeTablaEncontrada->nroDePagina);
@@ -131,28 +123,31 @@ st_registro* comandoSelect(st_select* comandoSelect, enum_resultados* resultado)
 			memcpy(registro->value, paginaDeTablaEncontrada->pagina+sizeof(double)+sizeof(uint16_t), tamanioValue);
 			sleep(configMemoria->RETARDO_MEM/1000);
             *resultado = OK;
+            log_info(file_log,"[proceso Select] Finalizando Proceso");
             pthread_mutex_unlock(&mutexMemPrinc);
 			return registro;
 		}
-        log_info(file_log, "[proces Select] No se Encontro Pagina");
-        log_info(file_log, "[proces Select] Buscar dato al FileSystem");
+        log_info(file_log, "[proceso Select] No se Encontro Pagina");
+        log_info(file_log, "[proceso Select] Buscar dato al FileSystem");
 		registro = obtenerSelect(comandoSelect);
 		if(registro == NULL){
-            log_info(file_log, "[proces Select] No se encontro dato en el FileSystem");
+            log_info(file_log, "[proceso Select] No se encontro dato en el FileSystem");
             *resultado = NOOK;
+            log_info(file_log,"[proceso Select] Finalizando Proceso");
 			pthread_mutex_unlock(&mutexMemPrinc);
 			return NULL;
 		}
-        log_info(file_log, "[proces Select] Buscando Disponibilidad en la memoria");
+        log_info(file_log, "[proceso Select] Buscando Disponibilidad en la memoria");
 		int posMarcoLibre = buscarMarcoLibre();
 
     	if(posMarcoLibre == -1){
-    		log_info(file_log, "[proces Select] Memoria full");
+    		log_info(file_log, "[proceso Select] Memoria full");
     		*resultado = FULLMEMORY;
+            log_info(file_log,"[proceso Select] Finalizando Proceso");
     		pthread_mutex_unlock(&mutexMemPrinc);
             return NULL;
     	}
-        log_info(file_log, "[proces Select] Cargando Nueva Pagina");
+        log_info(file_log, "[proceso Select] Cargando Nueva Pagina");
         void* paginaLibre = memoriaPrincipal + (posMarcoLibre * (sizeof(double) + sizeof(uint16_t) + tamanioValue));
 
 		memcpy(paginaLibre, &registro->timestamp, sizeof(double));
@@ -167,7 +162,7 @@ st_registro* comandoSelect(st_select* comandoSelect, enum_resultados* resultado)
 		paginaDeTabla->pagina = paginaLibre;
 		//paginaDeTabla->desplazamiento = string_length(registro->value);
 		paginaDeTabla->flagModificado = 0;
-        log_info(file_log, "[proces Select] Agregando la nueva pagina al segmento");
+        log_info(file_log, "[proceso Select] Agregando la nueva pagina al segmento");
 		list_add(segmentoEncontrado->tablaDePaginas, paginaDeTabla);
 
 		st_marco* marco = list_get(listaDeMarcos, posMarcoLibre);
@@ -175,34 +170,37 @@ st_registro* comandoSelect(st_select* comandoSelect, enum_resultados* resultado)
 		marco->timestamp = obtenerMilisegundosDeHoy();
         sleep(configMemoria->RETARDO_MEM/1000);
         *resultado = OK;
+        log_info(file_log,"[proceso Select] Finalizando Proceso");
         pthread_mutex_unlock(&mutexMemPrinc);
 		return registro;
 	}
-    log_info(file_log, "[proces Select] Segmento no encontrado");
-    log_info(file_log, "[proces Select] Buscando Disponibilidad en la memoria");
+    log_info(file_log, "[proceso Select] Segmento no encontrado");
+    log_info(file_log, "[proceso Select] Buscando Dato al FileSystem");
     registro = obtenerSelect(comandoSelect);
 
 	if(registro == NULL){
-        log_info(file_log, "[proces Select] No se encontro dato en el FileSystem");
+        log_info(file_log, "[proceso Select] No se encontro dato en el FileSystem");
         *resultado = NOOK;
+        log_info(file_log,"[proceso Select] Finalizando Proceso");
 		pthread_mutex_unlock(&mutexMemPrinc);
 		return NULL;
 	}
 
-    log_info(file_log, "[proces Select] Buscando Disponibilidad en la memoria");
+    log_info(file_log, "[proceso Select] Buscando Disponibilidad en la memoria");
     int posMarcoLibre = buscarMarcoLibre();
     if(posMarcoLibre == -1){
         log_info(file_log, "[proces Select] Memoria Full");
         *resultado = FULLMEMORY;
+        log_info(file_log,"[proceso Select] Finalizando Proceso");
         pthread_mutex_unlock(&mutexMemPrinc);
         return NULL;
     }
-    log_info(file_log, "[proces Select] Creando Nuevo segmento");
+    log_info(file_log, "[proceso Select] Creando Nuevo segmento");
     st_segmento* segmentoNuevo= malloc(sizeof(st_segmento));
 	segmentoNuevo->nombreTabla = strdup(comandoSelect->nameTable);
 	segmentoNuevo->tablaDePaginas = list_create();
 
-    log_info(file_log, "[proces Select] Cargando Nueva Pagina");
+    log_info(file_log, "[proceso Select] Cargando Nueva Pagina");
     void* paginaLibre = memoriaPrincipal + (posMarcoLibre * (sizeof(double) + sizeof(uint16_t) + tamanioValue));
 	memcpy(paginaLibre, &registro->timestamp, sizeof(double));
 	memcpy(paginaLibre + sizeof(double), &registro->key, sizeof(uint16_t));
@@ -217,9 +215,9 @@ st_registro* comandoSelect(st_select* comandoSelect, enum_resultados* resultado)
     //paginaDeTabla->desplazamiento = string_length(registro->value);
 	paginaDeTabla->flagModificado = 0;
 
-    log_info(file_log, "[proces Select] Agregando la nueva pagina al segmento");
+    log_info(file_log, "[proceso Select] Agregando la nueva pagina al segmento");
     list_add(segmentoNuevo->tablaDePaginas, paginaDeTabla);
-    log_info(file_log, "[proces Select] Agregando la nueva segmento a la lista de segmentos");
+    log_info(file_log, "[proceso Select] Agregando la nueva segmento a la lista de segmentos");
     segmentoNuevo->nroSegmento = list_size(listaDeSegmentos);
 	list_add_in_index(listaDeSegmentos, segmentoNuevo->nroSegmento, segmentoNuevo);
 	st_marco* marco = list_get(listaDeMarcos, posMarcoLibre);
@@ -227,6 +225,7 @@ st_registro* comandoSelect(st_select* comandoSelect, enum_resultados* resultado)
 	marco->timestamp = obtenerMilisegundosDeHoy();
     sleep(configMemoria->RETARDO_MEM/1000);
     *resultado = OK;
+    log_info(file_log,"[proceso Select] Finalizando Proceso");
     pthread_mutex_unlock(&mutexMemPrinc);
 	return registro;
 }
@@ -274,13 +273,11 @@ bool enviarSegmentoAFS(st_segmento* segmento){
 }
 
 int comandoJournal(){
-    //RECORRO TODA LA MEMORIA Y HAGO LOS INSERT CORRESPONDIENTES COMO REQUEST AL FILE
-	log_info(file_log, "Ejecutando Journal");
+	log_info(file_log, "[proceso Journal]Iniciando Journal");
 	bool resultado = true;
 	st_segmento * segmento;
-
     pthread_mutex_lock(&mutexMemPrinc);
-	for (int i = 0; i < list_size(listaDeSegmentos); i++){
+    for (int i = 0; i < list_size(listaDeSegmentos); i++){
     	segmento = list_get(listaDeSegmentos, i);
     	if(!enviarSegmentoAFS(segmento)){
     		resultado = false;
@@ -290,7 +287,7 @@ int comandoJournal(){
     }
 	//pthread_mutex_unlock(&mutexListaSeg);
     if(resultado){
-    log_info(file_log, "Termino el Journal");
+    log_info(file_log, "[proceso Journal]Finalizando el Journal");
     pthread_mutex_unlock(&mutexMemPrinc);
     return OK;
     }
@@ -298,7 +295,6 @@ int comandoJournal(){
     pthread_mutex_unlock(&mutexMemPrinc);
     return NOOK;
 }
-
 
 int removerSegmentoPorNombrePagina(char* nombreTabla){
 	pthread_mutex_lock(&mutexMemPrinc);
